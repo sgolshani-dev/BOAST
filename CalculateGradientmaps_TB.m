5b477ba951ff0578eaa18351225c915b812ef56a
sgolshn <s.golshani@ucl.ac.uk>
2025-03-29 13:53:58 +0000

    Update CalculateGradientmaps_TB.m

--- CalculateGradientmaps_TB.m
+++ CalculateGradientmaps_TB.m
@@ -14,13 +14,12 @@ function [fm_dX, fm_dY, fm_dZ] = CalculateGradientmaps_TB(file_fmp)
 %        file_fmp                 : Field map file (in Hz), with a resolution of 1 mm.
 % Output:
 %        fm_dX, fm_dY, fm_dZ      : Field Gradients in x,y,z directions in T/m 
-% =========================================================================                                   in x,y,z directions           
+% =========================================================================           
 % Updated 29/09/2024
 % By Shokoufeh Golshani
 
 
-gam = 2*pi*42.58*10^6;        % gyromagnetic ratio in Hz/Tesla for protons
-Larmor = gam/2/pi;
+gam = 42.58*10^6;        % gyromagnetic ratio in Hz/T for protons
 
 % =========================================================================
 % Read field map and calculate the gradient of the field
@@ -47,8 +46,8 @@ fm_dY = reshape(fm_dY, [fm_V.dim(1), fm_V.dim(2), fm_V.dim(3)]);
 fm_dZ = reshape(fm_dZ, [fm_V.dim(1), fm_V.dim(2), fm_V.dim(3)]);
 
 spm_progress_bar('Set', 6);
-fm_dX = fm_dX/vx_size(1)/Larmor;
-fm_dY = fm_dY/vx_size(2)/Larmor;
-fm_dZ = fm_dZ/vx_size(3)/Larmor;
+fm_dX = fm_dX/vx_size(1)/gam;
+fm_dY = fm_dY/vx_size(2)/gam;
+fm_dZ = fm_dZ/vx_size(3)/gam;
 
 end
